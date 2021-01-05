cc0da54c6e1b627bbba13352d9888e8f3c92e5d0
prof30 <mine4christ@gmail.com>
2021-01-05 07:48:47 -0800

    First commit

---
+++ app/src/main/java/com/ooemma/onyinyeobasi/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ooemma.onyinyeobasi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getPackageManager().getLaunchIntentForPackage("com.pas.webcam");
+        if(intent != null )
+        {
+            startActivity(intent);
+        }
+    }
+}
